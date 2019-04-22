0f960d677ad2527e1a041972e6c4956b1a404561
pocoz <felixmayakovsky@gmail.com>
2019-04-22 14:33:29 +0300

    Fix bug with fin containers

--- httpserver/service.go
+++ httpserver/service.go
@@ -49,69 +49,70 @@ func (s *basicService) createBuild(ctx context.Context, payload *types.Payload) 
 				continue
 			}
 
+			// Pull last container
+			out, err := s.dockerCli.ImagePull(context.TODO(), imageName, dtypes.ImagePullOptions{RegistryAuth: s.registryAuth})
+			if err != nil {
+				level.Info(s.logger).Log("method", "[image pull]container pull", "err", err)
+				continue
+			}
+			_, err = io.Copy(os.Stdout, out)
+			if err != nil {
+				level.Info(s.logger).Log("method", "[io copy]container pull", "err", err)
+				continue
+			}
+			err = out.Close()
+			if err != nil {
+				level.Info(s.logger).Log("method", "[out close]container pull", "err", err)
+				continue
+			}
+
 			// Get running containers
 			containers, err := s.dockerCli.ContainerList(context.Background(), dtypes.ContainerListOptions{})
 			if err != nil {
 				level.Info(s.logger).Log("method", "get containers", "err", err)
 				continue
 			}
-			for _, c := range containers {
-				// Pull last container
-				out, err := s.dockerCli.ImagePull(context.TODO(), imageName, dtypes.ImagePullOptions{RegistryAuth: s.registryAuth})
-				if err != nil {
-					level.Info(s.logger).Log("method", "[image pull]container pull", "err", err)
-					continue
-				}
-				_, err = io.Copy(os.Stdout, out)
-				if err != nil {
-					level.Info(s.logger).Log("method", "[io copy]container pull", "err", err)
-					continue
-				}
-				err = out.Close()
-				if err != nil {
-					level.Info(s.logger).Log("method", "[out close]container pull", "err", err)
-					continue
-				}
+			if len(containers) > 0 {
+				for _, c := range containers {
+					// Find running containers
+					if c.Image == imageName {
+						// Stop old container
+						duration := 15 * time.Second
+						err = s.dockerCli.ContainerStop(ctx, c.ID, &duration)
+						if err != nil {
+							level.Info(s.logger).Log("method", "container stop", "err", err)
+							continue
+						}
 
-				// Find running containers
-				if c.Image == imageName {
-					// Stop old container
-					duration := 15 * time.Second
-					err = s.dockerCli.ContainerStop(ctx, c.ID, &duration)
-					if err != nil {
-						level.Info(s.logger).Log("method", "container stop", "err", err)
-						continue
-					}
-
-					// Remove old container
-					err = s.dockerCli.ContainerRemove(ctx, c.ID, dtypes.ContainerRemoveOptions{})
-					if err != nil {
-						level.Info(s.logger).Log("method", "container remove", "err", err)
-						continue
+						// Remove old container
+						err = s.dockerCli.ContainerRemove(ctx, c.ID, dtypes.ContainerRemoveOptions{})
+						if err != nil {
+							level.Info(s.logger).Log("method", "container remove", "err", err)
+							continue
+						}
 					}
 				}
+			}
 
-				// Create new container
-				newContainer, err := s.dockerCli.ContainerCreate(
-					context.TODO(),
-					&dcontainer.Config{
-						Image: imageName,
-					},
-					&dcontainer.HostConfig{
-						NetworkMode: "host",
-					}, nil, e.Target.Repository)
-				if err != nil {
-					level.Info(s.logger).Log("method", "container create", "err", err)
-					continue
-				}
+			// Create new container
+			newContainer, err := s.dockerCli.ContainerCreate(
+				context.TODO(),
+				&dcontainer.Config{
+					Image: imageName,
+				},
+				&dcontainer.HostConfig{
+					NetworkMode: "host",
+				}, nil, e.Target.Repository)
+			if err != nil {
+				level.Info(s.logger).Log("method", "container create", "err", err)
+				continue
+			}
 
-				// Start new container
-				err = s.dockerCli.ContainerStart(ctx, newContainer.ID, dtypes.ContainerStartOptions{})
-				if err != nil {
-					level.Info(s.logger).Log("method", "container start", "err", err)
-					continue
-				}
-
+			// Start new container
+			err = s.dockerCli.ContainerStart(context.TODO(), newContainer.ID, dtypes.ContainerStartOptions{})
+			if err != nil {
+				level.Info(s.logger).Log("method", "container start", "err", err)
+				continue
 			}
 
 		}
