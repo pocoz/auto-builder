62121dbee4d2cde77da898ee6251df9e525f1f26
pocoz <felixmayakovsky@gmail.com>
2019-04-22 20:02:07 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -xe
 
-rm -fr build && mkdir -p build/amd64
+rm -fr dist && mkdir dist
+
+mkdir -p build/amd64
 GOOS=linux GOARCH=amd64 go build -o build/amd64/auto-builder cmd/auto-builderd/main.go
 
-rm -fr dist && mkdir dist
-rm -fr pkg-build && mkdir -p pkg-build/amd64
+mkdir -p pkg-build/amd64
 go-bin-deb generate -a amd64 -w pkg-build/amd64/ --version 0.1.0 -o dist/auto-builder-amd64.deb
+
+rm -fr build
+rm -fr pkg-build
